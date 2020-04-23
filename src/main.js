1c216de757ea9904464965f8eb6a3b3bd04d6852
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-23 14:43:12 -0700

    create 3 btn's each which will display a different random img

--- src/main.js
+++ src/main.js
@@ -1,23 +1,62 @@
 import { Shibe } from './shibe';
+import { Cat } from './shibe';
+import { Bird } from './shibe';
 import $ from 'jquery';
 import './styles.css';
 
 $(document).ready(function() {
+  // Shibe
   $('#shibeLocation').click(function() {
 
     (async () => {
       let shibe = new Shibe();
-      const response = await shibe.getShibe(location);
+      const response = await shibe.getShibe();
       getElements(response);
     })();
 
     function getElements(response) {
       if (response) {
         $('.showText').text("Enjoy this picture!"); 
-        $('.showShibe').html(` <img alt = "doggo" src = "https://cdn.shibe.online/shibes/${response}.jpg">`);
+        $('.showShibe').html(`<img alt = "doggo" src = "https://cdn.shibe.online/shibes/${response}.jpg">`);
       } else {
+        $('.showShibeError').html(`Apologies there was an error to diplay Shibe`);
+      }
+    }
+  });
+
+  // Cat
+  $('#catLocation').click(function() {
+
+    (async () => {
+      let cat = new Cat();
+      const response = await cat.getCat();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
         $('.showText').text("Enjoy this picture!"); 
-        $('.showShibe').html(`Apologies there was an error`);
+        $('.showShibe').html(`<img alt = "doggo" src = "https://cdn.shibe.online/shibes/${response}.jpg">`);
+      } else {
+        $('.showCatError').html(`Apologies there was an error to diplay Cat`);
+      }
+    }
+  });
+  // Bird
+  $('#birdLocation').click(function() {
+
+    (async () => {
+      let bird = new Bird();
+      const response = await bird.getBird();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $('.showText').text("Enjoy this picture!"); 
+        $('.showShibe').html(`<img alt = "doggo" src = "https://cdn.shibe.online/shibes/${response}.jpg">`);
+      }else {
+      $('.showBirdError').html(`Apologies there was an error to diplay Bird`);
       }
     }
   });
--- src/shibe.js
+++ src/shibe.js
@@ -1,7 +1,7 @@
 export class Shibe {
-  async getShibe(location) {
+  async getShibe() {
     try {
-      let response = await fetch(`https://cors-anywhere.herokuapp.com/shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]&appid=${process.env.API_KEY}`);
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]&appid=${process.env.DOG_API_KEY}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
@@ -9,7 +9,41 @@ export class Shibe {
         jsonifiedResponse = false;
       }
       return jsonifiedResponse;
-    } catch {
+    } catch(error) {
+      return false;
+    }
+  }
+}
+
+export class Cat {
+  async getCat() {
+    try {
+      let response = await fetch();
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
+
+export class Bird {
+  async getBird() {
+    try {
+      let response = await fetch();
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
       return false;
     }
   }
