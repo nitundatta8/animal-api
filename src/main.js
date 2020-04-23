5f305efe76004a981ad5c85a4dc04ef604d0dbb7
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-04-23 11:20:25 -0700

    update styles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { shibe } from './shibe';
+// import { shibe } from './shibe';
 import $ from 'jquery';
 import './styles.css';
 
@@ -6,8 +6,7 @@ $(document).ready(function() {
   $('#shibeLocation').click(function() {
 
     let request = new XMLHttpRequest();
-    const url = `http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
-    &appid=${process.env.API_KEY}`;
+    const url = `https://cors-anywhere.herokuapp.com/shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -20,8 +19,9 @@ $(document).ready(function() {
     request.send();
 
    const getElements = function(response) {
-      console.log(response);
-      $('.showShibe').text(`Enjoy this ${response.main.humidity} picture!`);
+      
+      console.log(response+ "  ------");
+       $('.showShibe').html(`Enjoy this picture! <img alt = "doggo" src = "https://cdn.shibe.online/shibes/${response}.jpg">`);
     }
   });
 });
